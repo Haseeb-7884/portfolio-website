935cebcd5593f3b908c75b13c38c810143d4e365
Haseeb-7884 <usmananees040@gmail.com>
2024-10-22 20:36:41 +0500

    init-basic-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function Header() {
+
+let HeaderContainerStyle = {
+    backgroundColor:"#fbf8f8",
+    height: "40vh",
+
+};
+
+  return (
+    <>
+      <div className="container_fluid" style={HeaderContainerStyle}>
+        <h1>Portfolio</h1>
+      </div>
+    </>
+  );
+}
+
+export default Header
